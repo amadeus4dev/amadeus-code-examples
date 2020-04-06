785f0b8100bac616f1a8811b428a8c9a897ef315
akshitsingla <akshit.singla@yahoo.com>
2020-04-05 23:13:18 -0400

    Adding support for trip_purpose_prediction API

---
+++ trip_purpose_prediction/v2/get/node/trip_purpose_prediction.js
@@ -0,0 +1,17 @@
+var Amadeus = require("amadeus");
+var amadeus = new Amadeus({
+  clientId: 'YOUR_API_KEY',
+  clientSecret: 'YOUR_API_SECRET'
+});
+
+// The passenger is traveling for leisure or business?
+amadeus.travel.predictions.tripPurpose.get({
+  originLocationCode: 'NYC',
+  destinationLocationCode: 'MAD',
+  departureDate: '2020-08-01',
+  returnDate: '2020-08-12'
+}).then(function (response) {
+  console.log(response);
+}).catch(function (response) {
+  console.error(response);
+});
