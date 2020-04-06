f9eddfbc2e40ed74b656569f041603504b2fa887
akshitsingla <akshit.singla@yahoo.com>
2020-04-05 22:40:32 -0400

    Adding support for flight_offers_search GET API

---
+++ flight_offers_search/v2/get/node/flight_offers_search.js
@@ -0,0 +1,17 @@
+var Amadeus = require("amadeus");
+var amadeus = new Amadeus({
+  clientId: 'YOUR_API_KEY',
+  clientSecret: 'YOUR_API_SECRET'
+});
+
+// Find the cheapest flights from SYD to BKK
+amadeus.shopping.flightOffersSearch.get({
+  originLocationCode: 'SYD',
+  destinationLocationCode: 'BKK',
+  departureDate: '2020-08-01',
+  adults: '2'
+}).then(function (response) {
+  console.log(response);
+}).catch(function (response) {
+  console.error(response);
+});
