a475b262c6b603c85b8b5a38fb5e94a9f4daeadc
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-05-22 13:06:59 +0200

    delete low-fare search examples

--- flight_offers_search/v1/get/curl/flight_offers_search.sh
+++
@@ -1,8 +0,0 @@
-# Authentication: $AMADEUS_CLIENT_ID & $AMADEUS_CLIENT_SECRET can be defined
-# in your environmental variables or directly in your script
-ACCESS_TOKEN=$(curl -H "Content-Type: application/x-www-form-urlencoded" \
-https://test.api.amadeus.com/v1/security/oauth2/token \
--d "grant_type=client_credentials&client_id=$AMADEUS_CLIENT_ID&client_secret=$AMADEUS_CLIENT_SECRET" \
-| grep access_token | sed 's/"access_token": "\(.*\)"\,/\1/' | tr -d '[:space:]')
-
-curl -X GET "https://test.api.amadeus.com/v1/shopping/flight-offers?origin=NYC&destination=MAD&departureDate=2020-10-01&max=2" -H "Authorization: Bearer $ACCESS_TOKEN" -k
