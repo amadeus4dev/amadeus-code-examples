8eb7c47e7686e9dffe14711f84784dd0de498171
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-09-10 09:36:07 +0200

    on demand flight status java example

---
+++ on_demand_flight_status/v2/get/Java SDK/OnDemandFlightStatus.java
@@ -0,0 +1,30 @@
+// How to install the library at https://github.com/amadeus4dev/amadeus-java
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.Response;
+import com.amadeus.resources.DatedFlight;
+
+public class OnDemandFlightStatus {
+
+  public static void main(String[] args) throws ResponseException {
+
+    Amadeus amadeus = AAmadeus
+        .builder("YOUR_AMADEUS_API_KEY","YOUR_AMADEUS_API_SECRET")
+        .build();
+
+    // Returns the status of a given flight
+    DatedFlight[] flightStatus = amadeus.schedule.flights.get(Params
+        .with("flightNumber", "319")
+        .and("carrierCode", "AZ")
+        .and("scheduledDepartureDate", "2021-03-13"));
+
+   if (flightStatus[0].getResponse().getStatusCode() != 200) {
+        System.out.println("Wrong status code: " + flightStatus[0].getResponse().getStatusCode());
+        System.exit(-1);
+    }
+
+    System.out.println(pointsOfInterest[0]);
+  }
+}
