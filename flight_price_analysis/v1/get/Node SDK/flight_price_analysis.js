acd54c82c1226a422f657de5aed25222ad5937f4
Alvaro Navarro <alvaro.navarro@amadeus.com>
2020-11-09 17:23:35 +0100

    Add Flight Price Analysis Node example

---
+++ flight_price_analysis/v1/get/Node SDK/flight_price_analysis.js
@@ -0,0 +1,17 @@
+var Amadeus = require("amadeus");
+var amadeus = new Amadeus({
+  clientId: 'YOUR_API_KEY',
+  clientSecret: 'YOUR_API_SECRET'
+});
+
+
+// Am I getting a good deal on this flight?
+amadeus.analytics.itineraryPriceMetrics.get({
+   originIataCode: 'MAD',
+   destinationIataCode: 'CDG',
+   departureDate: '2021-01-13',
+}).then(function (response) {
+  console.log(response);
+}).catch(function (response) {
+  console.error(response);
+});
