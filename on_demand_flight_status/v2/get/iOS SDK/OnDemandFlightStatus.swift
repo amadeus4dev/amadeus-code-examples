d2d8690a6415bf20ab531363c0f75e01a2c3599a
Alvaro Navarro <alvaro.navarro@amadeus.com>
2020-11-25 12:03:59 +0100

    Add new iOS examples

---
+++ on_demand_flight_status/v2/get/iOS SDK/OnDemandFlightStatus.swift
@@ -0,0 +1,18 @@
+import Amadeus
+
+var amadeus: Amadeus = Amadeus(
+    client_id: "YOUR_API_ID",
+    client_secret: "YOUR_API_SECRET"
+)
+
+amadeus.schedule.flights.get(params:["carrierCode":"AZ",
+    "flightNumber":"319",
+    "scheduledDepartureDate":"2021-03-13"],
+    onCompletion: { result in
+        switch result {
+        case let .success(response):
+            print(response.data)
+        case let .failure(error):
+            fatalError(error.localizedDescription)
+        }
+    })
---
+++ tours_and_activities/v1/get/tours_and_activities/iOS SDK/ToursActivities.swift
@@ -0,0 +1,17 @@
+import Amadeus
+
+var amadeus: Amadeus = Amadeus(
+    client_id: "YOUR_API_ID",
+    client_secret: "YOUR_API_SECRET"
+)
+
+amadeus.shopping.activities.get(params: ["latitude": "41.397158",
+    "longitude": "2.160873"], 
+    onCompletion: { result in
+        switch result {
+        case let .success(response):
+            print(response.data)
+        case let .failure(error):
+            fatalError(error.localizedDescription)
+        }
+    })
