55f09aadd62941fbfb173a4ce0b2bfd9a43916e6
Alvaro Navarro <alvaro.navarro@amadeus.com>
2020-11-25 12:03:59 +0100

    Add new iOS examples

---
+++ flight_price_analysis/v1/get/iOS SDK/FlightPriceAnalysis.swift
@@ -0,0 +1,18 @@
+import Amadeus
+
+var amadeus: Amadeus = Amadeus(
+    client_id: "YOUR_API_ID",
+    client_secret: "YOUR_API_SECRET"
+)
+
+amadeus.analytics.itineraryPriceMetrics.get(params: ["originIataCode": "MAD",
+    "destinationIataCode": "CDG",
+    "departureDate": "2021-01-15"],
+    onCompletion: { result in
+        switch result {
+        case let .success(response):
+            print(response.data)
+        case let .failure(error):
+            fatalError(error.localizedDescription)
+        }
+    })
