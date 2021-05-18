5b35bb999bd43accaf98c22b13a874a5be0b355a
Anna Tsolakou <tsolakou.a@gmail.com>
2021-05-18 10:48:12 +0200

    flight availabilities node

---
+++ flight_availabilities_search/v1/post/Node SDK/flight_availabilities.js
@@ -0,0 +1,33 @@
+var Amadeus = require("amadeus");
+var amadeus = new Amadeus({
+  clientId: 'YOUR_API_KEY',
+  clientSecret: 'YOUR_API_SECRET'
+});
+
+body = JSON.stringify({
+    "originDestinations": [
+        {
+            "id": "1",
+            "originLocationCode": "MIA",
+            "destinationLocationCode": "ATL",
+            "departureDateTime": {
+                "date": "2021-11-01"
+            }
+        }
+    ],
+    "travelers": [
+        {
+            "id": "1",
+            "travelerType": "ADULT"
+        }
+    ],
+    "sources": [
+        "GDS"
+    ]
+})
+
+amadeus.shopping.availability.flightAvailabilities.post(body).then(function (response) {
+  console.log(response);
+}).catch(function (response) {
+  console.error(response);
+});
