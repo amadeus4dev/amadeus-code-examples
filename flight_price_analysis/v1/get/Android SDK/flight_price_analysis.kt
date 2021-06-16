e9ac5f506260566b6d8fa27518e893e55c64ba8e
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-06-16 16:34:48 +0200

    hotel search price analysis and flight status

---
+++ flight_price_analysis/v1/get/Android SDK/flight_price_analysis.kt
@@ -0,0 +1,31 @@
+// How to install the library at https://github.com/amadeus4dev/amadeus-android
+
+import com.amadeus.android.Amadeus
+import com.amadeus.android.ApiResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import android.util.Log;
+
+val job = SupervisorJob()
+val scope = CoroutineScope(Dispatchers.Main + job)
+
+val amadeus = Amadeus.Builder(context)
+    .setClientId("REPLACE_BY_YOUR_API_KEY")
+    .setClientSecret("REPLACE_BY_YOUR_API_SECRET")
+    .build()
+
+scope.launch {
+  when (val analysis = amadeus.analytics.itineraryPriceMetrics.get(
+                originIataCode = "MAD",
+                destinationIataCode = "CDG",
+                departureDate = "2021-03-21")) {
+    is ApiResult.Success -> {
+      Log.d("Result", "${result.data}")
+    }
+    is ApiResult.Error -> {
+      // Handle your error
+    }
+  }
+}
