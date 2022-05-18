910f84fe2d43595995eb7ffc221b6d3904e20858
MinjiK <km.minjikim@gmail.com>
2022-05-18 14:31:47 +0200

    add node

---
+++ airport_routes/Node SDK/airport_routes.js
@@ -0,0 +1,12 @@
+var Amadeus = require("amadeus");
+var amadeus = new Amadeus({
+    clientId: 'YOUR_API_KEY',
+    clientSecret: 'YOUR_API_SECRET'
+});
+
+// Find all destinations served by CDG Airport
+amadeus.airport.directDestinations.get({
+    departureAirportCode: 'CDG',
+}).catch(function (response) {
+    console.error(response);
+});
