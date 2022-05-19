c7bc3b1ba616323c9c4fe17db7e8c2bc33c8c0c1
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-05-20 00:23:14 +0200

    add Java examples

---
+++ hotel_list/v1/get/hotels_by_hotels/Java SDK/HotelList.java
@@ -0,0 +1,25 @@
+// How to install the library at https://github.com/amadeus4dev/amadeus-java
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.Hotel;
+
+public class HotelList {
+
+  public static void main(String[] args) throws ResponseException {
+    Amadeus amadeus = Amadeus
+      .builder("YOUR_AMADEUS_API_KEY", "YOUR_AMADEUS_API_SECRET")
+      .build();
+
+    Hotel[] hotels = amadeus.referenceData.locations.hotels.byHotels.get(
+      Params.with("hotelIds", "ARPARARA"));
+
+    if (hotels[0].getResponse().getStatusCode() != 200) {
+      System.out.println("Wrong status code: " + hotels[0].getResponse().getStatusCode());
+      System.exit(-1);
+    }
+
+    System.out.println(hotels[0]);
+  }
+}
