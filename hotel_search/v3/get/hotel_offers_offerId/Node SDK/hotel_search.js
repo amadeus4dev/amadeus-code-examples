07b1b868ad84154ab250e1f898139995871a4e16
Anna Tsolakou <anna.tsolakou@amadeus.com>
2022-05-23 11:42:22 +0200

    add hotel search v3 python examples

--- hotel_search/v3/get/hotel_offers_offerId/Node SDK/hotel_search.js
+++ hotel_search/v3/get/hotel_offers_offerId/Node SDK/hotel_search.js
@@ -5,7 +5,7 @@ var amadeus = new Amadeus({
 });
 
 // Check offer conditions of a specific offer id
-amadeus.shopping.hotelOfferSearch('XXX').get()
+amadeus.shopping.hotelOfferSearch('63A93695B58821ABB0EC2B33FE9FAB24D72BF34B1BD7D707293763D8D9378FC3').get()
 .then(function (response) {
   console.log(response);
 }).catch(function (response) {
