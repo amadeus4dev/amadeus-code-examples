3052c1af7cc2dae125477e1de1ae2b57b5557aba
Anna Tsolakou <anna.tsolakou@amadeus.com>
2022-12-21 17:09:20 +0100

    update java example

--- flight_create_orders/v1/post/Java SDK/FlightCreateOrders.java
+++ flight_create_orders/v1/post/Java SDK/FlightCreateOrders.java
@@ -4,12 +4,17 @@ import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.FlightOfferSearch;
+import com.amadeus.resources.FlightPrice;
 import com.amadeus.resources.FlightOrder;
-import com.amadeus.resources.Traveler;
-import com.amadeus.resources.Traveler.Document;
-import com.amadeus.resources.Traveler.Phone;
+import com.amadeus.resources.FlightOrder.Traveler;
+import com.amadeus.resources.FlightOrder.Document.DocumentType;
+import com.amadeus.resources.FlightOrder.Phone.DeviceType;
+import com.amadeus.resources.FlightOrder.Name;
+import com.amadeus.resources.FlightOrder.Phone;
+import com.amadeus.resources.FlightOrder.Contact;
+import com.amadeus.resources.FlightOrder.Document;
 
-public class FlightCreateOrders {
+public class FlightSearch {
 
   public static void main(String[] args) throws ResponseException {
 
@@ -21,23 +26,23 @@ public class FlightCreateOrders {
 
     traveler.setId("1");
     traveler.setDateOfBirth("2000-04-14");
-    traveler.setName(traveler.new Name("JORGE", "GONZALES"));
+    traveler.setName(new Name("JORGE", "GONZALES"));
 
-    Traveler.Phone[] phone = new Phone[1];
-    phone[0] = traveler.new Phone();
+    Phone[] phone = new Phone[1];
+    phone[0] = new Phone();
     phone[0].setCountryCallingCode("33");
     phone[0].setNumber("675426222");
-    phone[0].setDeviceType("MOBILE");
+    phone[0].setDeviceType(DeviceType.MOBILE);
 
-    Traveler.Contact contact = traveler.new Contact();
+    Contact contact = new Contact();
     contact.setPhones(phone);
     traveler.setContact(contact);
 
-    Traveler.Document[] document = new Document[1];
-    document[0] = traveler.new Document();
-    document[0].setDocumentType("PASSPORT");
+    Document[] document = new Document[1];
+    document[0] = new Document();
+    document[0].setDocumentType(DocumentType.PASSPORT);
     document[0].setNumber("480080076");
-    document[0].setExpiryDate("2022-10-11");
+    document[0].setExpiryDate("2023-10-11");
     document[0].setIssuanceCountry("ES");
     document[0].setNationality("ES");
     document[0].setHolder(true);
@@ -48,16 +53,16 @@ public class FlightCreateOrders {
     System.out.println(travelerArray[0]);
 
     FlightOfferSearch[] flightOffersSearches = amadeus.shopping.flightOffersSearch.get(
-            Params.with("originLocationCode", "PAR")
-                    .and("destinationLocationCode", "NYC")
-                    .and("departureDate", "2022-11-01")
-                    .and("returnDate", "2022-11-08")
+            Params.with("originLocationCode", "MAD")
+                    .and("destinationLocationCode", "ATH")
+                    .and("departureDate", "2023-08-01")
+                    .and("returnDate", "2023-08-08")
                     .and("adults", 1)
                     .and("max", 3));
 
     // We price the 2nd flight of the list to confirm the price and the availability
     FlightPrice flightPricing = amadeus.shopping.flightOffersSearch.pricing.post(
-            flightOffersSearches[1]);
+            flightOffersSearches[0]);
 
     // We book the flight previously priced
     FlightOrder order = amadeus.booking.flightOrders.post(flightPricing, travelerArray);
